4f9cae543c5fa28bfb22c8ac7c18be100f1e8b63
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-28 14:42:48 +0200

    added: calculator

---
+++ src/main/java/org/lessons/java/Calculator.java
@@ -0,0 +1,29 @@
+package org.lessons.java;
+
+public class Calculator {
+
+    private float num1;
+    private float num2;
+
+    public Calculator(float num1, float num2){
+        this.num1 = 8;
+        this.num2 = 2;
+    }
+
+
+    public float add(float n1, float n2){
+        return n1+n2;
+    }
+
+    public float subtract(float n1, float n2){
+        return n1-n2;
+    }
+
+    public float multiply(float n1, float n2){
+        return n1*n2;
+    }
+
+    public float divide(float n1, float n2){
+        return n1/n2;
+    }
+}
