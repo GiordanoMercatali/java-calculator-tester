11e6a11a131b5e9c331eb37609cd09f115b7ab06
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-28 15:21:07 +0200

    removed: unused variabes added: calculator test, test add method

--- src/main/java/org/lessons/Calculator.java
+++ src/main/java/org/lessons/Calculator.java
@@ -2,28 +2,35 @@ package org.lessons;
 
 public class Calculator {
 
-    private float num1;
-    private float num2;
+    // private float num1;
+    // private float num2;
 
-    public Calculator(float num1, float num2){
-        this.num1 = 8;
-        this.num2 = 2;
-    }
+    // public Calculator(float num1, float num2){
+    //     this.num1 = num1;
+    //     this.num2 = num2;
+    // }
 
+    // public float getNum1(){
+    //     return num1;
+    // }
 
-    public float add(float n1, float n2){
+    // public float getNum2(){
+    //     return num2;
+    // }
+
+    public static float add(float n1, float n2){
         return n1+n2;
     }
 
-    public float subtract(float n1, float n2){
+    public static float subtract(float n1, float n2){
         return n1-n2;
     }
 
-    public float multiply(float n1, float n2){
+    public static float multiply(float n1, float n2){
         return n1*n2;
     }
 
-    public float divide(float n1, float n2){
+    public static float divide(float n1, float n2){
         return n1/n2;
     }
 }
--- src/test/java/org/lessons/CalculatorTest.java
+++ src/test/java/org/lessons/CalculatorTest.java
@@ -1,5 +1,13 @@
 package org.lessons;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
-    
+
+    @Test
+    void testAdd(){
+        float newNum = Calculator.add(1, 2);
+        assertEquals(3, newNum);
+    }
 }
