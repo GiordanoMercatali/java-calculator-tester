9cbf8e2fdda6c8a5afce8ac08730666f1e18a991
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-28 15:22:38 +0200

    added: test, subtraction, multiply, divide

--- src/test/java/org/lessons/CalculatorTest.java
+++ src/test/java/org/lessons/CalculatorTest.java
@@ -10,4 +10,22 @@ public class CalculatorTest {
         float newNum = Calculator.add(1, 2);
         assertEquals(3, newNum);
     }
+
+    @Test
+    void testSub(){
+        float newNum = Calculator.subtract(3, 2);
+        assertEquals(1, newNum);
+    }
+
+    @Test
+    void testMult(){
+        float newNum = Calculator.multiply(2, 2);
+        assertEquals(4, newNum);
+    }
+
+    @Test
+    void testDiv(){
+        float newNum = Calculator.divide(6, 2);
+        assertEquals(3, newNum);
+    }
 }
